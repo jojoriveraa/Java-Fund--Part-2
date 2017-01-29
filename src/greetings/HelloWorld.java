cd85014cbb354367cee066a445a7f8765ac3327a
Jorge Rivera <jojoriveraa@gmail.com>
2017-01-28 18:27:36 -0600

    Module Collections - Before

--- src/greetings/HelloWorld.java
+++ src/greetings/HelloWorld.java
@@ -1,14 +1,13 @@
 package greetings;
 
 import printing.BWCartridge;
-import printing.ColorCartridge;
-import printing.ICartridge;
 import printing.Printer;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		Printer<BWCartridge> printer = new Printer<BWCartridge>(true, "MY PRINTER", new BWCartridge());
+		
 		try {
 			printer.print(-1);
 		} catch (IllegalArgumentException e) {
@@ -16,6 +15,5 @@ public class HelloWorld {
 		} finally {
 			printer.TurnOff();
 		}
-
 	}
 }
