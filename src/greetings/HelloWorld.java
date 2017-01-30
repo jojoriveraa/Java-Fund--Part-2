72ee39a2b1bf33b84dee6a6afd86c10ef174acbd
Jorge Rivera <jojoriveraa@gmail.com>
2017-01-30 17:04:42 -0600

    Enumerations - Iterating - After

--- src/greetings/HelloWorld.java
+++ src/greetings/HelloWorld.java
@@ -7,9 +7,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		Printer<ColorCartridge> printer = new Printer<ColorCartridge>(true, "MY PRINTER", ColorCartridge.RED);
-		printer.loadPaper(5);
-		printer.print(3);
-		printer.outputPage(2);
+		
+		for(ColorCartridge cartridge : ColorCartridge.values()){
+			System.out.println(cartridge.printColor());
+		}
+		
 	}
 
 }
