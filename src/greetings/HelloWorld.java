af68e70804aae27bbcce8a3a10e06f21db7c1e39
Jorge Rivera <jojoriveraa@gmail.com>
2017-02-01 10:04:16 -0600

    Annotations - Reflection - After

--- src/greetings/HelloWorld.java
+++ src/greetings/HelloWorld.java
@@ -1,15 +1,26 @@
 package greetings;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import printing.ColorCartridge;
 import printing.Printer;
+import printing.PrintingDevice;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Printer<ColorCartridge> printer = new Printer<ColorCartridge>(true, "MY PRINTER", ColorCartridge.RED);
 
 		printer.loadPaper(10);
-		printer.outputPage(4);
+		
+		PrintingDevice annotation = printer.getClass().getAnnotation(PrintingDevice.class);
+		
+		Method printMethod = printer.getClass().getMethod(annotation.defaultPrintMethod(), int.class);
+				
+		printMethod.invoke(printer, annotation.defaultNumberOfCoppies());
+		
+		printer.outputPage(5);
 				
 	}
 
